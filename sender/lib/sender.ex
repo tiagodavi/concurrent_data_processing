b11d710df53d4522cbc6387f620550dfbfeb1e31
Tiago Davi <tiago.asp.net@gmail.com>
2022-02-18 16:08:15 -0300

    add sender project

---
+++ sender/lib/sender.ex
@@ -0,0 +1,21 @@
+defmodule Sender do
+  @moduledoc """
+  Documentation for `Sender`.
+  """
+
+  def send_email(email) do
+    Process.sleep(3_000)
+    IO.puts("Email to #{email} sent")
+    {:ok, email}
+  end
+
+  def notify_all(emails) do
+    emails
+    |> Enum.map(&Task.async(fn -> send_email(&1) end))
+    |> Enum.map(&Task.await/1)
+  end
+
+  def build_emails do
+    ["hello@world.com", "hola@world.com", "nihao@world.com", "konnichiwa@world.com"]
+  end
+end
