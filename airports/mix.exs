4d032c23babbc37d35aa02b00fa032837ca2f2fc
Tiago Davi <tiago.asp.net@gmail.com>
2022-10-07 14:48:59 -0300

    add flow

---
+++ airports/mix.exs
@@ -0,0 +1,29 @@
+defmodule Airports.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :airports,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Airports.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:flow, "~> 1.0"},
+      {:nimble_csv, "~> 1.1"}
+    ]
+  end
+end
